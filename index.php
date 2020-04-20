fccaa4461374f2a60c347e1481f3e7c121838f73
Max <eekumax@gmail.com>
2020-04-20 14:06:00 +0700

    php go

--- index.php
+++ index.php
@@ -634,7 +634,7 @@
           <div class='row'>
             <div class='col-sm-6'>
               <h1 class='section-title animated' data-animation-delay='50' data-animation='fadeInUp'>
-                Contacts
+                Contacts<br><br>
               </h1>
 
               <form action='#' method="POST" id='contact-form'>
